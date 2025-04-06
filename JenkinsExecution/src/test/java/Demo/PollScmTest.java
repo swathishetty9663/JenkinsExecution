d1248c14b63204ec7f4610d425f9c26999167221
08swa <08swa@96636880978660>
2025-04-06 12:12:38 +0530

    New class added

---
+++ JenkinsExecution/src/test/java/Demo/PollScmTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+	@Test
+	public void test() {
+		Reporter.log("poll scm class added",true);
+	}
+
+}
